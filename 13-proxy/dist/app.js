fbf15b07893d0479581c229f04ef20b22d0bf65c
Nikita Kaberov <enagtim@mail.ru>
2024-10-02 12:10:55 +0300

    Сделаны исправления

--- 13-proxy/dist/app.js
+++ 13-proxy/dist/app.js
@@ -1,8 +1,6 @@
 "use strict";
-// Класс для работы с реальным API
 class ProductAPI {
     async getProductById(id) {
-        // Логика запроса к реальному API
         if (id <= 10) {
             return {
                 success: true,
@@ -14,33 +12,32 @@ class ProductAPI {
         }
     }
 }
-// Прокси-класс для фильтрации запросов по ID
 class ProductProxy {
     constructor(api) {
         this.api = api;
     }
+    ;
     async getProductById(id) {
         if (id > 10) {
-            // Фильтрация запросов по ID
             return {
                 success: false,
                 message: 'Invalid product ID. Cannot retrieve products with ID greater than 10.'
             };
         }
         else {
-            // Проксирование реального запроса
             return await this.api.getProductById(id);
         }
     }
+    ;
 }
-// Пример использования прокси и API
+;
 (async () => {
     const api = new ProductAPI();
     const proxy = new ProductProxy(api);
     try {
-        const result1 = await proxy.getProductById(5); // Успешный запрос
+        const result1 = await proxy.getProductById(5);
         console.log('Product 1:', result1);
-        const result2 = await proxy.getProductById(15); // Ошибка, так как ID > 10
+        const result2 = await proxy.getProductById(15);
         console.log('Product 2:', result2);
     }
     catch (error) {
